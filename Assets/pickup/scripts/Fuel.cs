1ba8c580678224fea67391e80a6032a92374aae8
23hitth <23hitth@live.elida.k12.oh.us>
2020-12-03 10:42:04 -0500

    Commit

---
+++ Assets/pickup/scripts/Fuel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuel : MonoBehaviour
+{
+
+    [Header("Info")]
+    public float current = 100f;
+
+    [Header("Settings")]
+    public float max = 100f;
+    public float burnRate = 1f;
+
+    void Update()
+    {
+        current -= burnRate * Time.deltaTime;
+
+        if (current <= 0f)
+        {
+
+        }
+    }
+
+    void Refuel()
+    {
+        current = max;
+    }
+
+    void StopScript()
+    {
+        if (current < 0) ;
+        {
+            this.StopScript("Car");
+            bool Car = false;
+        }
+    }
+}
